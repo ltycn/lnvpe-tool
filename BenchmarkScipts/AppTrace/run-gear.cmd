45e68c1ac37c81208c68f6ab869979de94653432
劉天義 <i@terry.ee>
2024-06-28 18:26:50 +0800

    update

--- BenchmarkScipts/AppTrace/run-gear.cmd
+++ BenchmarkScipts/AppTrace/run-gear.cmd
@@ -2,20 +2,20 @@
 setlocal enabledelayedexpansion
 
 ::====================
-set "socketport="
+set "socketport=2"
 ::====================
 
 set "destPath=%USERPROFILE%\Desktop\AppTrace"
-set "countFilePath=%destPath%\Count.txt"
-set "messageFilePath=%destPath%\AppList.txt"
-set "resultFilePath=%destPath%\result.txt"
-set "scriptPath=%destPath%\run.cmd"
-set "APPTracePath=%destPath%\AppTrace.exe"
-set "runOnceKeyPath=HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 
 cd /d "%destPath%"
 
-
+set "gearFilePath=%destPath%\Gear.txt"
+set "countFilePath=%destPath%\Count.txt"
+set "messageFilePath=%destPath%\AppList.txt"
+set "resultFilePath=%destPath%\result.txt"
+set "scriptPath=%destPath%\run-gear.cmd"
+set "APPTracePath=%destPath%\AppTrace.exe"
+set "runOnceKeyPath=HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 
 for %%p in (
     standby-timeout-ac
@@ -28,6 +28,10 @@ for %%p in (
     powercfg /change "%%p" 0
 )
 
+if not exist "%gearFilePath%" (
+    echo 2 > "%gearFilePath%"
+)
+
 if not exist "%countFilePath%" (
     echo 0 > "%countFilePath%"
     reg add "%runOnceKeyPath%" /v AutoRestartScript /t REG_SZ /d "cmd.exe /c \"%scriptPath%\"" /f
@@ -36,6 +40,7 @@ if not exist "%countFilePath%" (
     shutdown /r /f /t 0
     exit /b
 )
+
 
 set /p count=<"%countFilePath%"
 set /a count+=1
@@ -54,8 +59,26 @@ for /f "tokens=1,* delims=:" %%a in ('findstr /n "^" "%messageFilePath%"') do (
 :continue
 
 if not defined message (
-    echo Test finished, Existing...
-    timeout /t 5 /nobreak
+    echo Test finished, Exiting...
+    set /p gear=<"%gearFilePath%"
+    echo ============ Setting Gear to Gear !gear! =========== >> "%resultFilePath%"
+    
+    if !gear! GTR 7 (
+        echo Test Finished.
+        "\\VM-SERVER\lnvpe-share\TOOL\AutoCharge.exe" %socketport% 1
+        exit /b 0
+    )
+
+    cd %USERPROFILE%\Desktop\DTTControl
+    "%USERPROFILE%\Desktop\DTTControl\DTTControl.exe" setgear !gear!
+    set /a gear+=1
+    echo !gear!>"%gearFilePath%"
+    echo Resetting count files...
+    del /F %countFilePath%
+    reg add "%runOnceKeyPath%" /v AutoRestartScript /t REG_SZ /d "cmd.exe /c \"%scriptPath%\"" /f
+    echo Reboot to continue...
+    timeout /t 20 /nobreak
+    shutdown /r /f /t 0
     exit /b 1
 )
 
@@ -73,9 +96,9 @@ start /b "" cmd /c ""%APPTracePath%" !message! >> "%resultFilePath%" 2>>&1"
 echo Wait for testing end...
 timeout /t 100 /nobreak
 
-taskkill /im "%fileName%" /im "EXCEL.EXE" /im "POWERPNT.EXE" /im "ML_Scenario.exe"
+taskkill /im "%fileName%" /im "EXCEL.EXE" /im "POWERPNT.EXE"
 
-timeout /t 10 /nobreak
+timeout /t 20 /nobreak
 
 ::============= DC Battery Check - Auto Charge =============
 "\\VM-SERVER\lnvpe-share\TOOL\AutoCharge.exe" %socketport%
